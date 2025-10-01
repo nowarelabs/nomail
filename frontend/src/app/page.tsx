a7ec2e948a3186196ced6f5f97f1b1db5897bdf7
omusamurai <9350722+Sylvance@users.noreply.github.com>
2025-10-01 13:26:39 +0300

    feat: add logo back

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -475,6 +475,14 @@ export default function Home() {
       )}
 
       <div className="flex items-center gap-2 px-3 py-2 border-b">
+        <div className="flex items-center gap-2">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail size-5">
+            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+            <polyline points="22,6 12,13 2,6"></polyline>
+          </svg>
+          <div className="text-xl font-bold">nomail</div>
+        </div>
+        <div className="h-6 border-r border-muted"></div>
         <div className="relative flex-1">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
             fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
